4edaa20deddc92ecfd1852004e047d5beb428d42
Sebastian Martinez <sebafernandez85@gmail.com>
2016-08-26 11:07:28 -0400

    agregada invocación a la vista AgregarVentas.xhtml desde el menú

--- MiniGestorDeVentas/build/generated-sources/ap-source-output/minigestorventas/entities/Usuario_.java
+++ MiniGestorDeVentas/build/generated-sources/ap-source-output/minigestorventas/entities/Usuario_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-20T17:40:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-08-26T11:00:40")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
